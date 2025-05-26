b67b3267cd95e6887204b21d1afcff61f532c4e0
Daniel M. <daniel@omniimpact.com>
2025-05-26 18:40:15 -0400

    add input timeout progress bars

--- Scripts/ui_rebinder.gd
+++ Scripts/ui_rebinder.gd
@@ -1,5 +1,9 @@
 extends Control
 
+var read_time: float = 0.0
+
+@export var Input_Read_Time: float = 1.0
+
 @export var Action_to_Rebind: String = "":
 	set(value):
 		Action_to_Rebind = value
@@ -11,11 +15,18 @@ extends Control
 		Action_Name = value
 		_ready()
 		pass
+
+@onready
+var read_progress: ProgressBar = $MarginContainer/HBoxContainer/HBoxContainer/ContainerCustom/ProgressBar
+@onready
+var button_custom: Button = $MarginContainer/HBoxContainer/HBoxContainer/ContainerCustom/ButtonCustom
 		
 func _ready() -> void:
-	var button_default: Button = $MarginContainer/HBoxContainer/HBoxContainer/ButtonDefault
+	var button_default: Button = $MarginContainer/HBoxContainer/HBoxContainer/ContainerDefault/ButtonDefault
 	var rebinder_label: Label = $MarginContainer/HBoxContainer/Label
+	
 	rebinder_label.text = ""
+	read_progress.max_value = Input_Read_Time
 	
 	InputMap.load_from_project_settings()
 	
@@ -35,4 +46,29 @@ func _ready() -> void:
 		
 	
 	
+	pass
+	
+func update_progress() -> void:
+	if read_time == 0:
+		return
+	if read_time > 0:
+		read_time = read_time-0.2
+	if read_time < 0:
+		read_time = 0
+	if read_time >= 0:
+		read_progress.value = read_time
+	if read_time == 0:
+		read_progress.visible = false
+		button_custom.disabled = false
+	pass
+
+func _on_timer_timeout() -> void:
+	update_progress()
+	pass
+
+func _on_button_custom_pressed() -> void:
+	read_time = Input_Read_Time
+	read_progress.value = read_time
+	button_custom.disabled = true
+	read_progress.visible = true
 	pass
