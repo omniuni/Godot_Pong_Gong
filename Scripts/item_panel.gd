051b44e40a9934dd9595407022b27b65e8c92ef5
Daniel Marcus <daniel@omniimpact.com>
2025-06-01 00:12:12 -0400

    let panels move to target positions

--- Scripts/item_panel.gd
+++ Scripts/item_panel.gd
@@ -15,6 +15,7 @@ var going_up = false
 var going_down = false
 var computed_vertical_velocity = 0
 var last_known_linear_velovity = 0
+var move_to_target
 
 @onready
 var light_top_rect: ColorRect = $RigidBodyPanel/LightTopRect
@@ -22,6 +23,8 @@ var light_top_rect: ColorRect = $RigidBodyPanel/LightTopRect
 var light_bottom_rect: ColorRect = $RigidBodyPanel/LightBottomRect
 @onready
 var panel_body: RigidBody2D = $RigidBodyPanel
+@onready
+var panel_collision_area: CollisionShape2D = $RigidBodyPanel/CollisionShapePanel
 
 signal kick(direction: Vector2)
 
@@ -31,11 +34,37 @@ func _ready() -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
+	if move_to_target != null:
+		request_move_to(move_to_target)
 	var linear_velocity_y = computed_vertical_velocity*Speed_In_Pixels
 	if linear_velocity_y != last_known_linear_velovity:
 		panel_body.linear_velocity.y = linear_velocity_y
 		last_known_linear_velovity = linear_velocity_y
 	panel_body.linear_velocity.x = 0
+	pass
+	
+func request_move_to(target_global_position: Vector2) -> void:
+	if move_to_target == null or move_to_target != target_global_position:
+		move_to_target = target_global_position
+	print("move requested to "+str(target_global_position))
+	var panel_center = panel_collision_area.global_position
+	print("panel center at: "+str(panel_center))
+	# Reduce jitter if it's close enough
+	if abs(target_global_position.y-panel_center.y) < 3:
+		request_move_stop()
+		return
+	if target_global_position.y < panel_center.y:
+		going_up = true
+	if target_global_position.y > panel_center.y:
+		going_down = true
+	compute_velocity()
+	pass
+	
+func request_move_stop() -> void:
+	going_up = false
+	going_down = false
+	move_to_target = null
+	compute_velocity()
 	pass
 	
 func _input(event: InputEvent) -> void:
